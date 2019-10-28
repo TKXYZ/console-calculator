7b68372986fc82df5f6beecaa9727b546f459227
TKXYZ <tktrannn@live.com>
2019-10-28 13:58:17 -0400

    Created basic console calculator.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,49 @@ namespace Console_Calculator
 	{
 		static void Main(string[] args)
 		{
-            Console.WriteLine("Hello, world!");
-		}
+            // Declares and initlaized var.
+            int num1 = 0;
+            int num2 = 0;
+
+            // Title.
+            Console.WriteLine("C# Console Calculator");
+            Console.WriteLine("---------------------\n");
+
+            // Prompts user for first number.
+            Console.WriteLine("Type a number, then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            // Prompts user for second number.
+            Console.WriteLine("Type another number, then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Prompts user for operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\t+ to Add");
+            Console.WriteLine("\t- to Subtract");
+            Console.WriteLine("\t* to Multiply");
+            Console.WriteLine("\t/ to Divide");
+
+            // Switch statement to do the math based on user input.
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "/":
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            // Wait for user response before closing.
+            Console.WriteLine("Press any key to close the Calculator");
+            Console.ReadKey();
+        }
 	}
 }
